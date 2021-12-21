dc74e6da848c95d90fd25436a99c6cba3cc1f09b
OlegZhur <olegodg@yandex.ru>
2021-12-21 17:43:30 +0300

    Initial commit

---
+++ cloud-storage-common/src/main/java/ru/geekbrains/storage/common/EmailAsk.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.storage.common;
+
+public class EmailAsk implements Message{
+    private String email;
+
+    public EmailAsk(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public TypeMessage getTypeMessage() {
+        return TypeMessage.EMAIL_ASK;
+    }
+}
+
---
+++ cloud-storage-server/src/main/java/ru/geekbrains/storage/server/AuthentificationProvider.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.storage.server;
+public interface AuthentificationProvider {
+    String getUsernameByLogin(String login);
+
+    String getPasswordByLogin(String login);
+
+    boolean isLoginUsed(String login);
+
+    boolean isEmailUsed(String login);
+
+    String getUuidByLogin(String login);
+
+    String getLoginByEmail(String email);
+
+    boolean newUser(String lastname, String name, String email, String login, String password, String uuid);
+
+    void connectBD();
+
+    void disconnectBD();
+}
